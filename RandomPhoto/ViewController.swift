bfcf05c221414e753dcc59bdf02fcaab3e5fbee2
ddilarakarakas <karakasdilara1@gmail.com>
2022-12-19 21:22:33 +0300

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Dilara Karakaş on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
